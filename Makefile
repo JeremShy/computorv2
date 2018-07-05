a1c3365544fdc9d05e0dfb4dfac91610b93f8da2
jcamhi <jeremshy@h3lp.fr>
2018-07-05 19:15:46 +0200

    Automatic commit

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 RESULT = computorv2
-SOURCES = src/types.ml src/utils.ml src/lexeme.ml src/lexer.ml src/nbr.ml src/operator.ml src/parser.ml src/main.ml
+SOURCES = src/types.ml src/utils.ml src/lexeme.ml src/lexer.ml \
+			src/simple_nbr.ml src/matrices.ml src/nbr.ml src/operator.ml src/parser.ml \
+			src/main.ml
 LIBS=Str
 OCAMLMAKEFILE = OCamlMakefile
 
--- src/parser.ml
+++ src/parser.ml
@@ -3,6 +3,11 @@
 	There can't be any symbol in this list, but there can be operators.
 *)
 
+let get_prioritary_operator lexeme =
+	if (lexeme#get_type = Types.Operator) &&
+		(lexeme#get_content = "*" || lexeme#get_content = "**" || lexeme#get_content = "/" || lexeme#get_content = "%" || lexeme#get_content = "^")
+
+
 let count_equals_symbols lst =
 	let rec recu lst n =
 		match lst with
@@ -17,7 +22,7 @@ let get_lvalue_and_rvalue lst =
 		match lst with
 		| hd::tl when (hd#get_type = Types.Symbole && hd#get_content = "=") -> recu tl lvalue [] false
 		| hd::tl -> if inlvalue = true then
-						recu tl (lvalue @ [hd]) 	[] 				inlvalue else
+						recu tl (lvalue @ [hd])	[] 				inlvalue else
 						recu tl lvalue 			(rvalue @ [hd])	inlvalue
 			| [] -> (lvalue, rvalue)
 	in
