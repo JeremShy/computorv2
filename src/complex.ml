71ed2bd2c0bae06c57ef057601dd4850a831ce1a
jcamhi <jeremshy@h3lp.fr>
2018-07-11 19:35:44 +0200

    Avant de tout changer

--- src/complex.ml
+++ src/complex.ml
@@ -5,4 +5,5 @@ object
 
 	method get_real_part : float = _real_part
 	method get_imaginary_part : float = _imaginary_part
+	method describe = "(" ^ (string_of_float _real_part) ^ " + " ^ (string_of_float _imaginary_part) ^ "i)"
 end
--- src/resolve.ml
+++ src/resolve.ml
@@ -1,13 +1,40 @@
 (* 11+ *)
 
+let rec do_addition buffer state =
+	match buffer with
+	| Entity.Nbr(n1)::Entity.Nbr(n2)::tl ->
+	begin
+		match n1 with
+		| Nbr.RealInteger(x) ->
+		begin
+			match n2 with
+			| Nbr.RealInteger(x2) -> (Entity.Nbr(Nbr.RealInteger(x + x2)))::tl
+			| Nbr.RealFloat(f) -> (Entity.Nbr(Nbr.RealFloat((float_of_int x) + x2)))::tl
+		end
+		| _ -> raise (Types.Execution_error "")
+	end
+	| _ -> raise (Types.Execution_error "Error : Can only add two numbers for the moment")
+
+	(* | Entity.Nbr(Nbr.RealInteger(x))::Entity.Nbr(Nbr.RealFloat(f))::fin -> (recu tl (Entity.Nbr((Nbr.RealFloat(f +. (float_of_int x))))::fin)) (* x + f *)
+	| Entity.Nbr(Nbr.RealInteger(x1))::Entity.Nbr(Nbr.RealInteger(x2))::fin -> (recu tl (Entity.Nbr(Nbr.RealInteger(x1 + x2))::fin)) (* x + x *)
+	| Entity.Nbr(Nbr.RealFloat(f))::Entity.Nbr(Nbr.RealInteger(x))::fin -> (recu tl (Entity.Nbr((Nbr.RealFloat(f +. (float_of_int x))))::fin))
+	| Entity.Nbr(Nbr.RealFloat(f1))::Entity.Nbr(Nbr.RealFloat(f2))::fin -> (recu tl (Entity.Nbr(Nbr.RealFloat(f1 +. f2))::fin)) *)
+	(* | _ -> () *)
+
+
 let resolve (expr:Entity.expression) state =
 	let rec recu expr (buffer: Entity.entity list) =
 		match expr with
 		| Entity.Nbr(a)::tl -> recu tl (Entity.Nbr(a)::buffer)
 		| Entity.Func(a)::tl -> recu tl (Entity.Func(a)::buffer)
-		(* | Operator(o)::tl ->
+		| Operator(o)::tl ->
+			begin
 			match o with
-			| Addition -> *)
+			| Addition ->
+						begin
+						end
+			| _ -> ()
+			end
 		| _ -> ()
 		in
 	recu expr []
