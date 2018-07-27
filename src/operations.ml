2e8d8e91ce776207ba021cd9cdf6f01984433b4b
Jeremy Camhi <jcamhi@42.fr>
2018-07-27 18:07:45 +0200

    Dividing an int by an int now gives a float. (This subject really isn't clear, gosh).
    Modified todo

--- src/operations.ml
+++ src/operations.ml
@@ -14,7 +14,7 @@ let sub n1 n2 =
 
 let div n1 n2 =
   match n1, n2 with
-  | (Nbr.RealInteger(i1), Nbr.RealInteger(i2)) -> if i2 = 0 then raise (Types.Execution_error "Division by 0") else Nbr.RealInteger(i1 / i2)
+  | (Nbr.RealInteger(i1), Nbr.RealInteger(i2)) -> if i2 = 0 then raise (Types.Execution_error "Division by 0") else Nbr.RealFloat((float_of_int i1) /. (float_of_int i2))
   | (Nbr.RealFloat(f1), Nbr.RealFloat(f2)) -> if f2 = 0. then raise (Types.Execution_error "Division by 0") else Nbr.RealFloat(f1 /. f2)
   | (Nbr.ComplexNbr(c1), Nbr.ComplexNbr(c2)) -> Complex_calculator.div c1 c2 (* Complex calculator has to define the type of the returned expression *)
   | _ -> failwith "Error div"
